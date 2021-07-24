c45856afee828d36f1e085e9a282cbb0a5ef754a
mishamyrt <misha@myrt.co>
2021-07-25 02:09:52 +0400

    Update theme

--- src/components/MultiInput.svelte
+++ src/components/MultiInput.svelte
@@ -1,87 +1,79 @@
 <script lang="ts">
-    import { UNITS_CONFIG, convertToMeter } from "../modules/units";
-    import type { UnitsType } from "../modules/units";
+  import { UNITS_CONFIG, convertToMeter } from "../modules/units";
+  import type { UnitsType } from "../modules/units";
 
-    export let value;
-    export let unit: UnitsType = "m";
-    export let placeholder = "";
-    let rawValue;
+  export let value;
+  export let unit: UnitsType = "m";
+  export let placeholder = "";
+  let rawValue;
 
-    function handleChange() {
-        value = convertToMeter(rawValue, unit);
-    }
+  function handleChange() {
+    value = convertToMeter(rawValue, unit);
+  }
 </script>
 
 <div class="multiInput">
-    <input
-        {placeholder}
-        type="number"
-        bind:value={rawValue}
-        on:keyup={handleChange}
-        class="multiInput-input input"
-    />
-    <span class="multiInput-value">{unit}</span>
-    <select
-        bind:value={unit}
-        on:change={handleChange}
-        class="multiInput-select"
-    >
-        {#each Object.keys(UNITS_CONFIG) as key}
-            <option value={key}>{key}</option>
-        {/each}
-    </select>
+  <input
+    {placeholder}
+    type="number"
+    bind:value={rawValue}
+    on:keyup={handleChange}
+    class="multiInput-input input"
+  />
+  <span class="multiInput-value">{unit}</span>
+  <select bind:value={unit} on:change={handleChange} class="multiInput-select">
+    {#each Object.keys(UNITS_CONFIG) as key}
+      <option value={key}>{key}</option>
+    {/each}
+  </select>
 </div>
 
 <style lang="scss">
-    .multiInput {
-        position: relative;
-        width: 100%;
-        &-input {
-            width: 100%;
-        }
+  .multiInput {
+    position: relative;
+    width: 100%;
 
-        &-value {
-            position: absolute;
-            right: 2px;
-            top: 8px;
-            width: 48px;
-            line-height: 1;
-            text-align: center;
-            z-index: 2;
-            pointer-events: none;
-        }
-
-        &-select {
-            position: absolute;
-            right: 0;
-            top: 2px;
-            background: transparent;
-            text-align: center;
-            padding: 0;
-            margin: 0;
-            width: 52px;
-            height: 34px;
-            color: transparent;
-            border-radius: 0 4px 4px 0;
-            background-color: transparent;
-            z-index: 1;
-            transition: background-color 0.3s ease-out;
-
-            &:hover {
-                transition: background-color 0s ease-out;
-                background-color: rgba(0, 122, 255, 0.18);
-            }
-        }
-
-        &::after {
-            content: "";
-            display: block;
-            position: absolute;
-            height: 32px;
-            width: 2px;
-            background-color: var(--color-accent);
-            top: 3px;
-            right: 50px;
-        }
+    &-input {
+      width: 100%;
     }
+
+    &-value {
+      position: absolute;
+      right: 2px;
+      top: 14px;
+      width: 48px;
+      line-height: 1;
+      text-align: center;
+      z-index: 2;
+      pointer-events: none;
+      color: #86868b;
+    }
+
+    &-select {
+      position: absolute;
+      right: 0;
+      top: 2px;
+      background: transparent;
+      text-align: center;
+      padding: 0;
+      margin: 0;
+      width: 51px;
+      height: 47px;
+      color: transparent;
+      border-radius: 0 12px 12px 0;
+      background-color: transparent;
+      z-index: 1;
+    }
+
+    &::after {
+      content: "";
+      display: block;
+      position: absolute;
+      height: 46px;
+      width: 1px;
+      background-color: var(--color-gray);
+      top: 3px;
+      right: 50px;
+    }
+  }
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,55 +1,63 @@
 <script lang="ts">
-    import ResolutionInput from "../components/ResolutionInput.svelte";
-    import MultiInput from "../components/MultiInput.svelte";
-    import {
-        calculateDPI,
-        calculateVisibilityFactor,
-    } from "../modules/display";
-    import { metersToInches } from "../modules/units";
+  import ResolutionInput from "../components/ResolutionInput.svelte";
+  import MultiInput from "../components/MultiInput.svelte";
+  import { calculateDPI, calculateVisibilityFactor } from "../modules/display";
+  import { metersToInches } from "../modules/units";
 
-    let distance = 0;
-    let size = 0;
-    let width = 0;
-    let height = 0;
+  let distance = 0;
+  let size = 0;
+  let width = 0;
+  let height = 0;
 
-    function getFactorClass(val: number) {
-        if (val > 0) {
-            return 1;
-        } else if (val >= -0.005) {
-            return 0;
-        } else {
-            return -1;
-        }
+  function getFactorClass(val: number) {
+    if (val > 0) {
+      return 1;
+    } else if (val >= -0.005) {
+      return 0;
+    } else {
+      return -1;
     }
+  }
 
-    $: density = calculateDPI(metersToInches(size), width, height);
-    $: visibilityFactor = getFactorClass(
-        calculateVisibilityFactor(distance, density)
-    );
+  $: density = calculateDPI(metersToInches(size), width, height);
+  $: visibilityFactor = getFactorClass(
+    calculateVisibilityFactor(distance, density)
+  );
 </script>
 
 <div class="fields">
-    <span class="fields-title">Distance:</span>
-    <MultiInput placeholder="3.5" bind:value={distance} />
-    <span class="fields-title">Size:</span>
-    <MultiInput placeholder="43" unit="in" bind:value={size} />
-    <span class="fields-title">Resolution:</span>
-    <ResolutionInput bind:width bind:height />
-    {#if density}
-        <span class="fields-title">DPI:</span>
-        <span>{Math.floor(density)}</span>
-        <span class="fields-title">Pixel visibility:</span>
-        {#if visibilityFactor === 1}
-            <span class="visibility __none">None</span>
-        {:else if visibilityFactor === 0}
-            <span class="visibility __low">Low</span>
-        {:else}
-            <span class="visibility __high">High</span>
-        {/if}
+  <span class="fields-title">Distance:</span>
+  <MultiInput placeholder="3.5" bind:value={distance} />
+  <span class="fields-title">Size:</span>
+  <MultiInput placeholder="43" unit="in" bind:value={size} />
+  <span class="fields-title">Resolution:</span>
+  <ResolutionInput bind:width bind:height />
+  {#if density}
+    <span class="fields-title">DPI:</span>
+    <span>{Math.floor(density)}</span>
+    <span class="fields-title">Pixel visibility:</span>
+    {#if visibilityFactor === 1}
+      <span class="visibility __none">None</span>
+    {:else if visibilityFactor === 0}
+      <span class="visibility __low">Low</span>
+    {:else}
+      <span class="visibility __high">High</span>
     {/if}
+  {/if}
 </div>
 <div class="description">
-    <p>Marketing companies are increasing demands on the screens, offering more and more affordable 4k screens. That's great, but do we really need these resolutions?</p>
-    <p>The resolution of your eyesight won't get any higher from this purchase.  With this calculator you can verify if you can see the difference between the screens in real life.</p>
-    <p>For example, if you watch a 4k 43" screen from 3 meters away, you won't notice the difference if it' replaced with a 1080p.</p>
+  <p>
+    Marketing companies are increasing demands on the screens, offering more and
+    more affordable 4k screens. That's great, but do we really need these
+    resolutions?
+  </p>
+  <p>
+    The resolution of your eyesight won't get any higher from this purchase.
+    With this calculator you can verify if you can see the difference between
+    the screens in real life.
+  </p>
+  <p>
+    For example, if you watch a 4k 43" screen from 3 meters away, you won't
+    notice the difference if it' replaced with a 1080p.
+  </p>
 </div>
