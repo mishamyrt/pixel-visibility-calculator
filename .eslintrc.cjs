94240e6ecf355452685b5da3080c880027d9d38b
mishamyrt <misha@myrt.co>
2021-07-26 00:59:55 +0400

    Add linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,17 +3,23 @@ const ts = require('typescript')
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'standard'],
 	plugins: ['svelte3', '@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
-		'svelte3/typescript': ts
+		'svelte3/typescript': ts,
+    'svelte3/ignore-styles': () => true,
 	},
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2019
 	},
+  rules: {
+    'import/first': 'off',
+    'space-before-function-paren': ['error', 'never'],
+    'no-multiple-empty-lines': 'off'
+  },
 	env: {
 		browser: true,
 		es2017: true,
--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,25 @@
-import preprocess from 'svelte-preprocess';
-import staticAdapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess'
+import staticAdapter from '@sveltejs/adapter-static'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-        adapter: staticAdapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
-        paths: {
-            base: "/pixel-visibility-calculator"
-        }
-	}
-};
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    adapter: staticAdapter({
+      // default options are shown
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    }),
+    paths: {
+      base: '/pixel-visibility-calculator'
+    }
+  }
+}
 
-export default config;
+export default config
