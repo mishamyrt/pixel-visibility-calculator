a3994c5c995e599f357d730d6880e8660edb4398
mishamyrt <misha@myrt.co>
2021-07-31 01:33:50 +0400

    Use SPA mode

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
       // default options are shown
       pages: 'build',
       assets: 'build',
-      fallback: null
+      fallback: 'index.html'
     }),
     paths: {
       base: '/pixel-visibility-calculator'
